b0724751ae4fa8b85cc5e469e567e772f4b5608c
Robert Wright <rwright.programmer@gmail.com>
2025-12-13 17:48:28 -0600

    Update NextJS to fix vulnerability

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -5,19 +5,19 @@ export default function Footer() {
     return (
       <footer id="socials" className="p-10 border-t border-gray-200 dark:border-gray-800">
         <div className="flex justify-center gap-8">
-            <a href="https://www.youtube.com/@rwrightDev" className="text-blue-500 hover:text-blue-700" aria-label="YouTube">
+            <a href="https://www.youtube.com/@rwrightDev" className="text-blue-500 hover:text-white" aria-label="YouTube">
                 <FaYoutube size={30} />
             </a>
-            <a href="https://x.com/rwrightDev" className="text-blue-500 hover:text-blue-700" aria-label="X">
+            <a href="https://x.com/rwrightDev" className="text-blue-500 hover:text-white" aria-label="X">
                 <FaXTwitter size={30} />
             </a>
-            <a href="https://www.instagram.com/rwrightdev/" className="text-blue-500 hover:text-blue-700" aria-label="Instagram">
+            <a href="https://www.instagram.com/rwrightdev/" className="text-blue-500 hover:text-white" aria-label="Instagram">
                 <FaInstagram size={30} />
             </a>
-            <a href="https://bsky.app/profile/rwrightdev.bsky.social" className="text-blue-500 hover:text-blue-700" aria-label="Bluesky">
+            <a href="https://bsky.app/profile/rwrightdev.bsky.social" className="text-blue-500 hover:text-white" aria-label="Bluesky">
                 <SiBluesky size={30} />
             </a>
-            <a href="https://www.tiktok.com/@rwrightdev" className="text-blue-500 hover:text-blue-700" aria-label="TikTok">
+            <a href="https://www.tiktok.com/@rwrightdev" className="text-blue-500 hover:text-white" aria-label="TikTok">
                 <FaTiktok size={30} />
             </a>
         </div>
